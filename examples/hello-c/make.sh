777be0e0bfbda915c0044836a4969e536ebba9df
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2023-02-22 13:57:43 +0900

    Implement sign extension, stack pointer, etc.
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- examples/hello-c/make.sh
+++ examples/hello-c/make.sh
@@ -6,5 +6,5 @@ set -eux
 # ./configure --prefix=/opt/riscv --with-arch=rv32ia --with-abi=ilp32
 # make linux
 
-/opt/riscv/bin/riscv32-unknown-linux-gnu-gcc -march=rv32i -mabi=ilp32 -O3 -static -o hello-riscv32 hello.c 
+/opt/riscv/bin/riscv32-unknown-linux-gnu-gcc -march=rv32i -mabi=ilp32 -O3 -static -o hello-riscv32 hello.c
 # /opt/riscv/bin/riscv32-unknown-linux-gnu-strip hello-riscv32
